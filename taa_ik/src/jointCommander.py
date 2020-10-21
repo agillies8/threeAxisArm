2d0bfbc97088bc2409ffc563cdd6d716e582d9df
agillies8 <andrew.gillies@gmail.com>
2020-10-21 00:44:23 -0700

    added fwd k model, GUI command teleop working

--- taa_ik/src/jointCommander.py
+++ taa_ik/src/jointCommander.py
@@ -3,20 +3,30 @@ import rospy
 from std_msgs.msg import Float64
 from math import cos, sin
 
+#rosrun rqt_ez_publisher rqt_ez_publisher  Run this command to bring up the joint commander and send commmands over the three axis
+
 def joint():
     rospy.init_node('command', anonymous=True, log_level=rospy.WARN)
     pub1 = rospy.Publisher('command1', Float64, queue_size=10)
     pub2 = rospy.Publisher('command2', Float64, queue_size=10)
+    pub3 = rospy.Publisher('command3', Float64, queue_size=10)
 
 
-    rate = rospy.Rate(50) # 10hz
+    rate = rospy.Rate(50) # 50hz
     while not rospy.is_shutdown():
+
+
         joint1v = cos(rospy.get_time())
-        joint2v = sin(rospy.get_time())
+        joint2v = cos(rospy.get_time())
+        joint3v = sin(rospy.get_time())
+
         rospy.loginfo(joint1v)
         rospy.loginfo(joint2v)
+        rospy.loginfo(joint3v)
+
         pub1.publish(joint1v)
         pub2.publish(joint2v)
+        pub3.publish(joint3v)
 
         rate.sleep()
 
--- taa_ik/src/jointSubscriber.py
+++ taa_ik/src/jointSubscriber.py
@@ -3,17 +3,35 @@ import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 
-class CommandToJointState:
+#  0  base_axis1_joint - axis 1 pivot  n
+#  1  base_link1_joint - axis 2 input n 
+#  2  base_link3_joint - axis 3 input (secondary link) n
+#  3  link1_trilink_joint - axis 2 to triangle y 
+#  4  base_link6_joint y
+#  5  link3_link4_joint  - axis 3 to secondary y 
+#  6  tri_link5_joint - trinalge to top link y 
+#  7  link5_end_joint y 
+#  8  tri_link2_joint - lower link to ee
+
+class JointPub:
     def __init__(self):
-        self.joint_name = "link5_end_joint"
         self.joint_state = JointState()
-        self.joint_state.name.append(self.joint_name)
-        self.joint_state.position.append(0.0)
-        self.joint_state.velocity.append(0.0)
+        self.joint_state.name = ['base_axis1_joint', 'base_link1_joint','base_link3_joint', 'link1_trilink_joint', 'base_link6_joint', 'link3_link4_joint', 'tri_link5_joint', 'link5_end_joint', 'tri_link2_joint']
+        self.joint_state.position = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
+        self.joint_state.velocity = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
         self.joint_pub = rospy.Publisher("joint_states", JointState, queue_size=1)
 
-    def Pub(self, joint1, joint2):
-        self.joint_state.position[0] = joint1 +joint2
+    def Pub(self, joint1, joint2, joint3):
+        self.joint_state.position[0] = joint1 
+        self.joint_state.position[1] = joint2
+        self.joint_state.position[2] = joint3
+        self.joint_state.position[3] = - joint2
+        self.joint_state.position[4] = joint2
+        self.joint_state.position[5] =   joint2 - joint3
+        self.joint_state.position[6] = self.joint_state.position[8]
+        self.joint_state.position[7] = -joint3
+        self.joint_state.position[8] =  joint3
+
         self.joint_state.header.stamp = rospy.Time.now()
         self.joint_pub.publish(self.joint_state)
 
@@ -24,21 +42,26 @@ class MySubscriber(object):
         # TODO : do a wait topic        
         self.joint1 = 0.0
         self.joint2 = 0.0
-        self.joint3Pub = CommandToJointState()
+        self.joint3 = 0.0
+        self.jointpub = JointPub()
 
         rospy.Subscriber('command1', Float64, self.joint1_callback)
         rospy.Subscriber('command2', Float64, self.joint2_callback)
+        rospy.Subscriber('command3', Float64, self.joint3_callback)
 
     def joint1_callback(self,msg):
-        rospy.loginfo('got encoder %f', msg.data)
         self.joint1 = msg.data
+        self.jointpub.Pub(joint1=self.joint1, joint2=self.joint2, joint3=self.joint3)
 
     def joint2_callback(self,msg):
         # This callback is the boss, this one dictates the publish rate
-        rospy.loginfo('got joint2 position %f', msg.data)
         self.joint2 = msg.data
-        self.joint3Pub.Pub(joint1=self.joint1,
-                                            joint2=self.joint2)
+        self.jointpub.Pub(joint1=self.joint1, joint2=self.joint2, joint3=self.joint3)
+
+    def joint3_callback(self,msg):
+        # This callback is the boss, this one dictates the publish rate
+        self.joint3 = msg.data
+        self.jointpub.Pub(joint1=self.joint1, joint2=self.joint2, joint3=self.joint3)
 
 
     def loop(self):
